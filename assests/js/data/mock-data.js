1e286fa04d8e192277187a22c5ab06b34b4cbe8b
francijrjr <frjuniorfrjr@gmail.com>
2026-05-25 21:14:49 -0300

    feat: mokdata users

---
+++ assests/js/data/mock-data.js
@@ -0,0 +1,9 @@
+const MockData = {
+    user: {
+        id: 'user1',
+        email: 'francis@admin.com',
+        password: 'admin@123',
+        name: 'Francis',
+        role: 'admin',
+    }
+}
